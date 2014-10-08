ce2600d9869737d36fb8832cb36c9ccdb1beb980
Parag Nemade <pnemade@redhat.com>
2014-10-08 22:26:41 +0530

    We don't do any package resolving in langavailable. Mark it False

--- langpacks.py
+++ langpacks.py
@@ -355,7 +355,7 @@ class LangavailableCommand(dnf.cli.Command):
 
     def configure(self, args):
         demands = self.cli.demands
-        demands.resolving = True
+        demands.resolving = False
         demands.root_user = False
         demands.sack_activation = True
 
