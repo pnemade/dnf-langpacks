8bad02e88f3c85080b58c2d1a06fa2e9dca0238c
Parag Nemade <pnemade@redhat.com>
2014-11-20 21:11:00 +0530

    some code cleanup (indentation) as per pylint report

--- langpacks.py
+++ langpacks.py
@@ -240,19 +240,19 @@ class LangpackCommon(object):
 
             if len(lang) > 4:
                 if lang.find("_") != -1:
-                    mainlang = lang[0:lang.find("_")]
-                    if lang.find("_CN") != -1 and pkgs.find("-CN") != -1:
-                        avl_langpack_pkgs.append(pkgs)
-                    if pkgs.find(lang, len(pkgs)-len(lang), len(pkgs)) > 0:
-                        avl_langpack_pkgs.append(pkgs)
-                    # if input pt_BR then show for pt and pt_BR
-                    # if input zh_CN then show for zh and zh_CN
-                    elif pkgs.find(mainlang, len(pkgs)-len(mainlang), len(pkgs)) > 0:
-                        avl_langpack_pkgs.append(pkgs)
+                   mainlang = lang[0:lang.find("_")]
+                   if lang.find("_CN") != -1 and pkgs.find("-CN") != -1:
+                       avl_langpack_pkgs.append(pkgs)
+                   if pkgs.find(lang, len(pkgs)-len(lang), len(pkgs)) > 0:
+                       avl_langpack_pkgs.append(pkgs)
+                   # if input pt_BR then show for pt and pt_BR
+                   # if input zh_CN then show for zh and zh_CN
+                   elif pkgs.find(mainlang, len(pkgs)-len(mainlang), len(pkgs)) > 0:
+                       avl_langpack_pkgs.append(pkgs)
 
-                    if lname:
-                        if pkgs.find(lname, len(pkgs)-len(lname), len(pkgs)) > 0:
-                            avl_langpack_pkgs.append(pkgs)
+                   if lname:
+                      if pkgs.find(lname, len(pkgs)-len(lname), len(pkgs)) > 0:
+                          avl_langpack_pkgs.append(pkgs)
 
         return sorted(avl_langpack_pkgs)
 
