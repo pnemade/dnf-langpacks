1786a7e4a44c9a9f1f7b1665fc0c720806257f10
Parag Nemade <pnemade@redhat.com>
2015-02-02 17:57:59 +0530

    Update NEWS and bump version to 0.7.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Build script for dnf-langpacks
 from setuptools import setup, find_packages
 
 setup (name = "dnf-langpacks",
-    version = '0.6.1',
+    version = '0.7.0',
     packages = find_packages(), 
     description = "Automatic installation of langpacks of packages being installed.",
     author = 'Parag Nemade',
@@ -14,7 +14,8 @@ setup (name = "dnf-langpacks",
     platforms=["Linux"],
 
     data_files=[('/usr/lib/python2.7/site-packages/dnf-plugins', ['langpacks.py']),
-                 ('/usr/share/man/man8', ['dnf-langpacks.8'])],
+                 ('/usr/share/man/man8', ['dnf-langpacks.8']),
+                 ('/etc/dnf/plugins/', ['langpacks.conf'])],
 
     classifiers=['License :: OSI Approved ::  GNU General Public License (GPL)',
                  'Operating System :: Unix',
