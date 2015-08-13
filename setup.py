d61d1619658e1ea2b9a8e3fe9480d92bba6a2764
Parag Nemade <pnemade@redhat.com>
2015-08-13 13:28:58 +0530

    Update NEWS, setup.py, man page and bump release to 0.15.0

--- setup.py
+++ setup.py
@@ -4,21 +4,26 @@ Build script for dnf-langpacks
 """
 from setuptools import setup, find_packages
 
-setup (name = "dnf-langpacks",
-    version = '0.14.0',
-    packages = find_packages(), 
-    description = "Automatic installation of langpacks of packages being installed.",
-    author = 'Parag Nemade',
-    author_email = 'pnemade@redhat.com',
-    license = 'GPLv2+',
-    platforms=["Linux"],
+setup(name="dnf-langpacks",
+      version='0.15.0',
+      packages=find_packages(),
+      description="Automatic installation of langpacks of packages being installed.",
+      author='Parag Nemade',
+      author_email='pnemade@redhat.com',
+      license='GPLv2+',
+      platforms=["Linux"],
 
-    data_files=[('/usr/lib/python2.7/site-packages/dnf-plugins', ['langpacks.py']),
-                 ('/usr/share/man/man8', ['dnf.plugin.langpacks.8']),
-                 ('/etc/dnf/plugins/', ['langpacks.conf'])],
+      data_files=[('/usr/lib/python2.7/site-packages/dnf-plugins', ['langpacks.py']),
+                  ('/usr/share/man/man8', ['dnf.plugin.langpacks.8']),
+                  ('/etc/dnf/plugins/', ['langpacks.conf'])],
 
-    classifiers=['License :: OSI Approved ::  GNU General Public License (GPL)',
-                 'Operating System :: Unix',
-                 'Programming Language :: Python',
-                 ],
-)
+      classifiers=['License :: OSI Approved ::  GNU General Public License (GPL)',
+                   'Operating System :: Unix',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
+                   ],
+      )
