c1f08fd6818eef0b42a26e8c678d5d6e67f391dd
Parag Nemade <pnemade@redhat.com>
2015-09-10 11:33:52 +0530

    Update NEWS and bump release to 0.15.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Build script for dnf-langpacks
 from setuptools import setup, find_packages
 
 setup(name="dnf-langpacks",
-      version='0.15.0',
+      version='0.15.1',
       packages=find_packages(),
       description="Automatic installation of langpacks of packages being installed.",
       author='Parag Nemade',
